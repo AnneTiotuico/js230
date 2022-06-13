05bd6130746f8d9cac3dd06981422f3f478e8d32
Anne Tiotuico <amtiotuico@gmail.com>
2022-06-13 16:42:30 -0700

    refactored

--- lesson5/photo_gallery_slideshow/public/javascripts/gallery.js
+++ lesson5/photo_gallery_slideshow/public/javascripts/gallery.js
@@ -1,15 +1,22 @@
 $(function () {
   let slideshow = $(".slideshow");
   let thumbnails = $(".thumbnails");
+
   thumbnails.on('click', 'li', function(e) {
-    slideshow.find('figure.visible').fadeOut(200, function() {
-      $(this).removeClass('visible').addClass('hidden');
-    })
+    let currentImg = slideshow.find('figure:visible');
     let img = $(this).find('img').attr('src');
     let chosen = slideshow.find(`img[src='${img}']`).closest('figure');
-    chosen.delay(200).fadeIn(200, function() {
-      $(this).removeClass('hidden').addClass('visible');
+
+    if (currentImg.is(chosen)) return;
+
+    currentImg.fadeOut(200, function() {
+      $(this).toggleClass('hidden');
     });
+    
+    chosen.delay(200).fadeIn(200, function() {
+      $(this).toggleClass('hidden');
+    });
+    
     $('li').removeClass('active');
     $(this).addClass('active');
   })
